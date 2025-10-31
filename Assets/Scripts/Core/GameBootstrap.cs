67cf63475e5ec52cf8c6ce311d2b1ceae27f0c83
xemmis <atrogardey1@gmail.com>
2025-10-31 13:14:16 +0300

    interactionService

--- Assets/Scripts/Core/GameBootstrap.cs
+++ Assets/Scripts/Core/GameBootstrap.cs
@@ -12,9 +12,14 @@ public class GameBootstrap : MonoBehaviour
     [SerializeField] private CoffeeMachine _coffeMachine;
     [SerializeField] private float _coffeeCookTime = 3f;
 
+    [Header("Interaction Components")]
+    [SerializeField] private InteractionScannerService _interactionScannerService;
+    [SerializeField] private InteractionRayScanner _interactionRayScanner;
+
     private void Awake()
     {
         _dialogueSystem.Initialize(_dialogueVisualizer, _dialogueInputHandler);
         _coffeMachine.Initialize(new CoffeeCookingService(_coffeeCookTime));
+        _interactionScannerService.Initialize(_interactionRayScanner);
     }
 }
